008c403ca909f28528265ead1648d88528eee8af
katkinso <katyratkinson@yahoo.com>
2019-12-01 17:26:33 -0800

    mono

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -11,10 +11,10 @@ const cors = require("cors");
 module.exports = {
   init(app, express){
     // Serve static files from the React frontend app
-    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.use(express.static(path.join(__dirname,'..', '../client/build')))
     // Anything that doesn't match the above, send back index.html
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname + '/client/build/index.html'))
+      res.sendFile(path.join(__dirname, '..', '../client/build/index.html'))
     })
     // app.set("views", viewsFolder);
     // app.set("view engine", "ejs");
