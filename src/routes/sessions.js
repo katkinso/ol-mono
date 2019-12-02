586c2722d095634397b928fd0952072665b40a18
katkinso <katyratkinson@yahoo.com>
2019-12-01 18:53:28 -0800

    api

--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -5,7 +5,7 @@ const helper = require("../auth/helpers");
 const sessionController = require("../controllers/sessionController")
 
 router.post("/api/sessions/create", helper.ensureAuthenticated, sessionController.create);
-router.get("/api/sessions", helper.ensureAuthenticated, sessionController.all);
+router.get("/api-sessions", helper.ensureAuthenticated, sessionController.all);
 router.get("/api/sessions/:id", helper.ensureAuthenticated, sessionController.detail);
 router.post("/api/sessions/register", helper.ensureAuthenticated, sessionController.registration);
 router.delete("/api/sessions/:id", helper.ensureAuthenticated, sessionController.delete);
