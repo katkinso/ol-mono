b8d3d1fdd61693c66638b8023ccf8eed4eb4f67b
katkinso <katyratkinson@yahoo.com>
2019-12-01 18:42:05 -0800

    routes

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -49,7 +49,7 @@ module.exports = {
       app.use(express.static(path.join(__dirname,'..', '../client/build')))
 
     // Handle React routing, return all requests to React app
-      app.get(['/profile', '/dashboard', '/sessions'], function(req, res) {
+      app.get(['/profile', '/dashboard', '/session'], function(req, res) {
         res.sendFile(path.join(__dirname, '..', '../client/build/index.html'))
       });
     }
